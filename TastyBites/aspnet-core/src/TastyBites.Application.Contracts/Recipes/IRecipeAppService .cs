feddc85008c926e03e2328f700b8a4c5f08e684a
MibAX <Instructors@mibax.com>
2024-10-10 23:35:32 +0200

    04.10 - Defining the Service Interface

---
+++ TastyBites/aspnet-core/src/TastyBites.Application.Contracts/Recipes/IRecipeAppService .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace TastyBites.Recipes
+{
+    public interface IRecipeAppService : ICrudAppService<
+             RecipeDto,
+             int,
+             PagedAndSortedResultRequestDto>
+    {
+
+        // Ceate, Read(single) + Read(List), Update, Delete
+    }
+}
