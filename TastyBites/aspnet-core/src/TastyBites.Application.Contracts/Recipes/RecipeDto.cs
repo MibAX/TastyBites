6a9c8f07fba8d5fdc6a7adc9b593510f9fdac7f8
MibAX <Instructors@mibax.com>
2024-10-10 22:54:24 +0200

    04.08 - Creating the Data Transfer Object (DTO)

---
+++ TastyBites/aspnet-core/src/TastyBites.Application.Contracts/Recipes/RecipeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace TastyBites.Recipes
+{
+    public class RecipeDto : EntityDto<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
