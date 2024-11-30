8aa92d79f47b6a60159fd8207c68e903564181e8
MibAX <Instructors@mibax.com>
2024-11-30 10:44:53 +0100

    09.23 - Refactoring buildForm

--- TastyBites/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
+++ TastyBites/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
@@ -20,12 +20,14 @@ export class CrudRecipeComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('CrudRecipeComponent > ngOnInit');
+    this.buildFrom();
+  }
 
+  private buildFrom() {
     this.recipeFormGroup = this.fb.group({
       name: [''],
       description: ['']
     });
-
   }
 
   cancel(): void {
