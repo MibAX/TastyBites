42d2fa46c3156a99b0469fa699fa41ac62d23857
MibAX <Instructors@mibax.com>
2025-02-22 19:43:06 +0100

    10.19 Updating Recipe Routes for Better UX

--- TastyBites/admin.angular/src/app/recipes/recipes-list/recipes-list.component.ts
+++ TastyBites/admin.angular/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -26,11 +26,11 @@ export class RecipesListComponent implements OnInit {
   }
 
   newRecipe(): void {
-    this.router.navigate(["/recipes/crud"])
+    this.router.navigate(["/recipes/create"])
   }
 
   editRecipe(id: number): void {
-    this.router.navigate(["/recipes/crud", id])
+    this.router.navigate(["/recipes/edit", id])
   }
 
 }
